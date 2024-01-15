aed9c759365392e1ff9e95624b8137a61a773628
Liam Hauw <liamhauw@gmail.com>
2024-01-15 22:05:45 +0800

    Remove uesless classes

--- engine/engine.cc
+++ engine/engine.cc
@@ -16,15 +16,11 @@ Engine::Engine()
       time_{std::make_shared<Time>()},
       window_{std::make_shared<Window>(time_)},
       function_input_{std::make_shared<FunctionInput>(context_, window_)},
-      physics_{std::make_shared<Physics>()},
-      world_{std::make_shared<World>()},
-      camera_{std::make_shared<Camera>()},
       gpu_{std::make_shared<Gpu>(window_)},
       scene_graph_{std::make_shared<SceneGraph>(asset_, gpu_)},
       rendering_{std::make_shared<Rendering>(window_, gpu_)},
       function_ui_{std::make_shared<FunctionUi>()},
       editor_input_{std::make_shared<EditorInput>(context_, window_)},
-      scene_{std::make_shared<Scene>()},
       editor_ui_{std::make_shared<EditorUi>()} {}
 
 void Engine::Run() {
@@ -35,15 +31,11 @@ void Engine::Run() {
     time_->Tick();
     window_->Tick();
     function_input_->Tick();
-    physics_->Tick();
-    world_->Tick();
-    camera_->Tick();
     gpu_->Tick();
     scene_graph_->Tick();
     rendering_->Tick();
     function_ui_->Tick();
     editor_input_->Tick();
-    scene_->Tick();
     editor_ui_->Tick();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(2));
--- engine/engine.h
+++ engine/engine.h
@@ -9,18 +9,14 @@
 
 #include "editor/editor_input/editor_input.h"
 #include "editor/editor_ui/editor_ui.h"
-#include "editor/scene/scene.h"
-#include "function/camera/camera.h"
 #include "function/context/context.h"
 #include "function/function_input/function_input.h"
 #include "function/function_ui/function_ui.h"
 #include "function/gpu/gpu.h"
-#include "function/physics/physics.h"
 #include "function/rendering/rendering.h"
 #include "function/scene_graph/scene_graph.h"
 #include "function/time/time.h"
 #include "function/window/window.h"
-#include "function/world/world.h"
 #include "resource/asset/asset.h"
 #include "resource/config/config.h"
 
@@ -39,15 +35,11 @@ class Engine {
   std::shared_ptr<Time> time_;
   std::shared_ptr<Window> window_;
   std::shared_ptr<FunctionInput> function_input_;
-  std::shared_ptr<Physics> physics_;
-  std::shared_ptr<World> world_;
-  std::shared_ptr<Camera> camera_;
   std::shared_ptr<Gpu> gpu_;
   std::shared_ptr<SceneGraph> scene_graph_;
   std::shared_ptr<Rendering> rendering_;
   std::shared_ptr<FunctionUi> function_ui_;
   std::shared_ptr<EditorInput> editor_input_;
-  std::shared_ptr<Scene> scene_;
   std::shared_ptr<EditorUi> editor_ui_;
 };
 
